40cd910aee79a1db2412be55b7a04877d91653d5
Devang Manjramkar <devangmanjramkar@gmail.com>
2023-05-19 22:58:43 +0530

    Footer componentized

--- pages/app.tsx
+++ pages/app.tsx
@@ -233,46 +233,7 @@ const Apps: NextPage = () => {
         </div>
       </div>
 
-      <div className="footer">
-        <div className="container">
-          <div className="row">
-            <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-              <div className="copyright">
-                <p>
-                  © Copyright <span id="year"></span>{" "}
-                  <Link href="#">Tendex</Link> I All Rights Reserved
-                </p>
-              </div>
-            </div>
-            <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-              <div className="footer-social">
-                <ul>
-                  <li>
-                    <Link href="#">
-                      <i className="bi bi-facebook"></i>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="#">
-                      <i className="bi bi-twitter"></i>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="#">
-                      <i className="bi bi-linkedin"></i>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="#">
-                      <i className="bi bi-youtube"></i>
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <FooterComponent />
     </LandingLayout>
   );
 };
---
+++ src/components/SideBarComponent.tsx
@@ -0,0 +1,88 @@
+import React from "react";
+import Link from "next/dist/client/link";
+import Image from "next/legacy/image";
+import logoi from "../public/images/logoi.png";
+
+type SideBarComponentProps = {};
+
+const SideBarComponent: React.FC<SideBarComponentProps> = () => {
+  return (
+    <div className="sidebar">
+      <div className="brand-logo">
+        <Link href="/">
+          <Image src={logoi} alt="" width={30} />
+        </Link>
+      </div>
+      <div className="menu">
+        <ul>
+          <li>
+            <Link
+              href="dashboard"
+              data-toggle="tooltip"
+              data-placement="right"
+              title="Home"
+            >
+              <span>
+                <i className="bi bi-house"></i>
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="trade"
+              data-toggle="tooltip"
+              data-placement="right"
+              title="Trade"
+            >
+              <span>
+                <i className="bi bi-globe2"></i>
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="wallet"
+              data-toggle="tooltip"
+              data-placement="right"
+              title="Wallet"
+            >
+              <span>
+                <i className="bi bi-wallet2"></i>
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="settings-profile"
+              data-toggle="tooltip"
+              data-placement="right"
+              title="Settings"
+              id="settings"
+            >
+              <span>
+                <i className="bi bi-gear"></i>
+              </span>
+            </Link>
+          </li>
+          <li className="logout">
+            <Link
+              href="signin"
+              data-toggle="tooltip"
+              data-placement="right"
+              title="Signout"
+            >
+              <span>
+                <i className="bi bi-power"></i>
+              </span>
+            </Link>
+          </li>
+        </ul>
+
+        <p className="copyright">
+          &#169; <Link href="#">Qkit</Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+export default SideBarComponent;
